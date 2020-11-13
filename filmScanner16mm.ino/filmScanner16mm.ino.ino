cb240fc9e5142d07b3579dabd280fb6019328b63
Tuomo Rainio <tuomo.rainio@gmail.com>
2020-11-13 22:45:46 +0200

    New structureand functions

--- filmScanner16mm.ino/filmScanner16mm.ino.ino
+++ filmScanner16mm.ino/filmScanner16mm.ino.ino
@@ -23,36 +23,219 @@
 
 
 // A) Antava Stepperi 1
-
 const int PULSE_1 = 2; // STEP 1
 const int DIRECTION_1 = 3; // DIRECTION 1
 const int ENABLE_1 = 4; // ENABLE 1
 
 // B) Ottava Stepperi 2
-
 const int PULSE_2     = 5; // STEP 2
 const int DIRECTION_2 = 6; // DIRECTION 2
 const int ENABLE_2    = 7; // ENABLE 2
 
 // C) Gate/spro Stepper 3
-
 const int PULSE_3     = 8; // ENABLE 3
 const int DIRECTION_3 = 9; // ENABLE 3
 const int ENABLE_3    = 10; // STEP 3
 
-// Kytkimet
-const int FRAME_DETECTION = 52;
+// Buttons/Switches
+const int PIN_PLAY_BUTTON = 50;
+const int PIN_FRAME_DETECTION_SWITCH = 52; 
+// TODO: Add some new buttons here!
+// BUTTON_KELAUS_ETEEN
+// BUTTON_KELAUS_TAAKSE
+// BUTTON_PLAY_ETEEN
+// BUTTON_PLAY_TAAKSE
+// BUTTON_STOP
+// BUTTON_REC
 
+// modes
+int mode = 0; // 0=stopped, 1=rewind, 2=play
+bool recording = false;
 
-bool backwards = false;
+// running directions (true = forwards, false = backwards)
+bool running_forward = true;
 
-int frame_detected = false;
-int kierrokset=0;
+// other global variables
+int saved_frames_count=0;
+
 
 void setup() {
   
   Serial.begin(9600);
   
+  setPinModes();
+  
+  enableAllMotors();
+
+  if(mode == 1)
+  {
+    disableGateMotor();
+  }
+  
+  if(running_forward==true)
+  {
+    setDirectionForwards();
+  }
+  else
+  {
+    setDirectionBackwards();
+  }
+  
+}
+
+
+void loop() 
+{
+  switch (mode)
+  {
+    case 1:
+      rewinding(100);
+      break;
+    case 2:
+      move_one_frame(100);
+      if(recording == true)
+      {
+        Serial.println("ota kuva!");
+        // TODO: send signal via lanc
+        // TODO: wait until frame is saved. How long time? Knob for adjusting time?
+        Serial.print("Number of saved frames :"+String(saved_frames_count));
+        // count the images  
+        saved_frames_count++;
+      }
+      // pause always after single frame is moved
+      delay(1000);
+      break;
+    default:
+      // stopped, no need to move anywhere
+      break;
+  }
+
+  // read inputs from control panel
+  readPlayControl();
+  // TODO: readRewindControl(); > set mode
+  // TODO: readDirectionControl(); > set direction
+  
+  // print info
+  //TODO: print info to lcd
+
+}
+
+
+void move_one_frame(int pulse_delay)
+{
+  for (int i=0; i<5000; i++)    //Forward 5000 steps
+  {
+  
+  move_one_step(pulse_delay);
+  
+  // Do not detect switch state until motor has rotated
+  if(i>150)
+  {
+    // read the state of the frame detection switch value:
+    int frame_detected = digitalRead(PIN_FRAME_DETECTION_SWITCH);
+    
+    // if switch is pressed break out from for loop
+    if(frame_detected == LOW)
+    {
+        // break out from loop when frame is detected
+        Serial.println("frame detected!");
+        i= 5000;
+    }
+  }
+  
+  }
+}
+
+void play_pulseHIGH()
+{
+  digitalWrite(PULSE_1,HIGH);
+  digitalWrite(PULSE_2,HIGH);
+  digitalWrite(PULSE_3,HIGH);
+}
+ 
+void play_pulseLOW()
+{
+  digitalWrite(PULSE_1,LOW);
+  digitalWrite(PULSE_2,LOW);
+  digitalWrite(PULSE_3,LOW);
+}
+
+void rewind_pulseHIGH()
+{
+  digitalWrite(PULSE_1,HIGH);
+  digitalWrite(PULSE_2,HIGH);
+}
+ 
+void rewind_pulseLOW()
+{
+  digitalWrite(PULSE_1,LOW);
+  digitalWrite(PULSE_2,LOW);
+}
+
+void move_one_step(int pulse_delay)
+{
+  play_pulseHIGH();
+  delayMicroseconds(50);
+  play_pulseLOW();
+  delayMicroseconds(pulse_delay);
+}
+
+void rewinding(int pulse_delay)
+{
+  rewind_pulseHIGH();
+  delayMicroseconds(50);
+  rewind_pulseLOW();
+  delayMicroseconds(pulse_delay);
+  
+}
+
+void setDirectionForwards()
+{
+  digitalWrite(DIRECTION_1,HIGH);
+  digitalWrite(DIRECTION_2,HIGH);
+  digitalWrite(DIRECTION_3,HIGH);
+}
+
+void setDirectionBackwards()
+{
+  digitalWrite(DIRECTION_1,LOW);
+  digitalWrite(DIRECTION_2,LOW);
+  digitalWrite(DIRECTION_3,LOW);
+}
+
+void enableAllMotors()
+{
+  digitalWrite(ENABLE_1,LOW);
+  digitalWrite(ENABLE_2,LOW);
+  digitalWrite(ENABLE_3,LOW);
+}
+
+void disableGateMotor()
+{
+  digitalWrite(ENABLE_3,HIGH);
+}
+
+
+
+void readPlayControl()
+{
+  int play_state = digitalRead(PIN_PLAY_BUTTON);
+
+ if(play_state == LOW)
+    {
+        Serial.println("Run!");
+        mode = 2;
+    }
+    else
+    {
+        Serial.println("Stop running!");
+        mode = 0;
+    }
+}
+
+
+
+void setPinModes(){
   pinMode (PULSE_1, OUTPUT);
   pinMode (DIRECTION_1, OUTPUT);
   pinMode (ENABLE_1, OUTPUT);
@@ -65,72 +248,7 @@ void setup() {
   pinMode (DIRECTION_3, OUTPUT);
   pinMode (ENABLE_3, OUTPUT);
 
-  pinMode (FRAME_DETECTION, INPUT_PULLUP);
-  
-  
-  if(backwards){
-      
-      digitalWrite(DIRECTION_1,LOW);
-      digitalWrite(ENABLE_1,LOW);
-      digitalWrite(DIRECTION_2,LOW);
-      digitalWrite(ENABLE_2,LOW);
-      digitalWrite(DIRECTION_3,LOW);
-      digitalWrite(ENABLE_3,LOW);
-  } 
-  else
-  {
-      digitalWrite(DIRECTION_1,HIGH);
-      digitalWrite(ENABLE_1,LOW);
-      digitalWrite(DIRECTION_2,HIGH);
-      digitalWrite(ENABLE_2,LOW);
-      digitalWrite(DIRECTION_3,HIGH);
-      digitalWrite(ENABLE_3,LOW);
-  }
-
+  pinMode (PIN_FRAME_DETECTION_SWITCH, INPUT_PULLUP);
+  pinMode (PIN_PLAY_BUTTON, INPUT_PULLUP);
 }
-
-
-void loop() {
-
-  for (int i=0; i<5000; i++)    //Forward 5000 steps
-  {
-    
-    digitalWrite(PULSE_1,HIGH);
-    digitalWrite(PULSE_2,HIGH);
-    digitalWrite(PULSE_3,HIGH);
-
-    delayMicroseconds(100);
-
-    digitalWrite(PULSE_1,LOW);
-    digitalWrite(PULSE_2,LOW);
-    digitalWrite(PULSE_3,LOW);
-    
-    delayMicroseconds(100);
-
-      // Do not detect switch state until motor has rotated
-      if(i>150)
-      {
-      // read the state of the frame detection switch value:
-      frame_detected = digitalRead(FRAME_DETECTION);
   
-          // if switch is pressed break out from for loop
-          if(frame_detected == LOW)
-          {
-              Serial.println("frame detected!");
-               i= 5000;
-          }
-      }
-      
-    }
-    
-  // count the images  
-  kierrokset++;
-
-  
-  Serial.println("ota raw kuva!");
-  Serial.print("Frameja on :"+String(kierrokset));
-
-  // wait between frames
-  delay(1000);
-  
-  }
